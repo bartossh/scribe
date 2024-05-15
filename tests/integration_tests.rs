ffec8951bf536385438b5e8fcfd14378854fb482
Bartossh <lenart.consulting@gmail.com>
2024-05-15 10:29:04 +0200

    Add integration tests for API query correctness.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -5,7 +5,7 @@ use serde::Serialize;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::io::Result;
+use std::io::{Error, Result};
 use std::thread::sleep;
 use std::time::Duration;
 use std::time::Instant;
@@ -58,6 +58,535 @@ fn mix_and_merge(rng: &mut ThreadRng, rounds: usize, data: &[String]) -> String 
 
 #[test]
 #[ignore]
+fn on_create_log_api_call_should_respond_with_code_200() -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&LogInput {
+            log: "I am the log that is added in to application".to_string(),
+        });
+    match status {
+        Ok(resp) => assert_eq!(resp.status(), 200),
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+    sleep(Duration::from_millis(WAIT_MS));
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn on_read_log_api_call_match_words_and_prefix_should_use_multiple_query_params_and_respond_with_logs_matching_all_query_params(
+) -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let time_from = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    for l in vec![
+        "This log is number one prefix match",
+        "This log is number two",
+        "This log is number three prefix match",
+        "This log is number four",
+        "Not found one",
+        "Not found two but prefix match",
+        "Not found three",
+        "Not found four but prefix match",
+        "Not found five",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    let time_to = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+
+    sleep(Duration::from_millis(10));
+
+    for l in vec![
+        "Outside of time range. This log is number one prefix match",
+        "Outside of time range. This log is number two prefix match",
+        "Outside of time range. This log is number three prefix match",
+        "Outside of time range. This log is number four prefix match",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    let path = "http://0.0.0.0:8000/read";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&Query {
+            prefix: Some("pref".to_string()),
+            words: Some(vec![
+                "This".to_string(),
+                "log".to_string(),
+                "is".to_string(),
+                "number".to_string(),
+            ]),
+            from: time_from.as_nanos() as u64,
+            to: time_to.as_nanos() as u64,
+        });
+    sleep(Duration::from_millis(10));
+
+    match status {
+        Ok(resp) => {
+            assert_eq!(resp.status(), 200);
+            let logs: LogsOutput = resp.into_json()?;
+            assert_eq!(logs.logs.len(), 2);
+            for w in "This log is number".split_whitespace() {
+                for log in logs.logs.iter() {
+                    let sl: Vec<String> = log.split_whitespace().map(str::to_string).collect();
+                    let Some(_) = sl.iter().find(|s| *s == w) else {
+                        assert!(false);
+                        return Err(Error::new(std::io::ErrorKind::NotFound, "item not found"));
+                    };
+                }
+            }
+        }
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn on_read_log_api_call_match_existing_word_should_respond_with_previously_added_log_that_match_the_query(
+) -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let time_from = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    for l in vec![
+        "This log is number one",
+        "This log is number two",
+        "This log is number three",
+        "This log is number four",
+        "Not found one",
+        "Not found two",
+        "Not found three",
+        "Not found four",
+        "Not found five",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    let time_to = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+
+    sleep(Duration::from_millis(100));
+
+    let path = "http://0.0.0.0:8000/read";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&Query {
+            prefix: None,
+            words: Some(vec![
+                "This".to_string(),
+                "log".to_string(),
+                "is".to_string(),
+                "number".to_string(),
+            ]),
+            from: time_from.as_nanos() as u64,
+            to: time_to.as_nanos() as u64,
+        });
+    match status {
+        Ok(resp) => {
+            assert_eq!(resp.status(), 200);
+            let logs: LogsOutput = resp.into_json()?;
+            assert_eq!(logs.logs.len(), 4);
+            for w in "This log is number".split_whitespace() {
+                for log in logs.logs.iter() {
+                    let sl: Vec<String> = log.split_whitespace().map(str::to_string).collect();
+                    let Some(_) = sl.iter().find(|s| *s == w) else {
+                        assert!(false);
+                        return Err(Error::new(std::io::ErrorKind::NotFound, "item not found"));
+                    };
+                }
+            }
+        }
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn on_read_log_api_call_match_existing_word_should_respond_with_empty_message_when_logs_do_not_match(
+) -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let time_from = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    for l in vec![
+        "Not found one",
+        "Not found two",
+        "Not found three",
+        "Not found four",
+        "Not found five",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    let time_to = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+
+    sleep(Duration::from_millis(100));
+
+    let path = "http://0.0.0.0:8000/read";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&Query {
+            prefix: None,
+            words: Some(vec![
+                "This".to_string(),
+                "log".to_string(),
+                "is".to_string(),
+                "number".to_string(),
+            ]),
+            from: time_from.as_nanos() as u64,
+            to: time_to.as_nanos() as u64,
+        });
+    match status {
+        Ok(resp) => {
+            assert_eq!(resp.status(), 200);
+            let logs: LogsOutput = resp.into_json()?;
+            assert_eq!(logs.logs.len(), 0);
+        }
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn on_read_log_api_call_match_word_prefix_should_respond_with_messages_when_logs_has_matching_prefix(
+) -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let time_from = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    for l in vec![
+        "This log is super funny should found",
+        "This log is supper fun should found",
+        "This log is supper fan",
+        "This log is supper fantastic",
+        "This log is supper fonetic",
+        "This log is supper fixed",
+        "This log is supper fu should found",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    let time_to = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+
+    sleep(Duration::from_millis(100));
+
+    let path = "http://0.0.0.0:8000/read";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&Query {
+            prefix: Some("fu".to_string()),
+            words: None,
+            from: time_from.as_nanos() as u64,
+            to: time_to.as_nanos() as u64,
+        });
+    match status {
+        Ok(resp) => {
+            assert_eq!(resp.status(), 200);
+            let logs: LogsOutput = resp.into_json()?;
+            assert_eq!(logs.logs.len(), 3);
+            for w in "should found".split_whitespace() {
+                for log in logs.logs.iter() {
+                    let sl: Vec<String> = log.split_whitespace().map(str::to_string).collect();
+                    let Some(_) = sl.iter().find(|s| *s == w) else {
+                        assert!(false);
+                        return Err(Error::new(std::io::ErrorKind::NotFound, "item not found"));
+                    };
+                }
+            }
+        }
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn on_read_log_api_call_match_word_prefix_should_respond_with_empty_response_when_prefix_is_not_matching(
+) -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let time_from = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    for l in vec![
+        "This log is super funny should found",
+        "This log is supper fun should found",
+        "This log is supper fan",
+        "This log is supper fantastic",
+        "This log is supper fonetic",
+        "This log is supper fixed",
+        "This log is supper fu should found",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    let time_to = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+
+    sleep(Duration::from_millis(100));
+
+    let path = "http://0.0.0.0:8000/read";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&Query {
+            prefix: Some("xx".to_string()),
+            words: None,
+            from: time_from.as_nanos() as u64,
+            to: time_to.as_nanos() as u64,
+        });
+    match status {
+        Ok(resp) => {
+            assert_eq!(resp.status(), 200);
+            let logs: LogsOutput = resp.into_json()?;
+            assert_eq!(logs.logs.len(), 0);
+        }
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn on_read_log_api_call_match_time_should_find_all_in_time_range() -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let time_from = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    for l in vec![
+        "This log is super funny should found",
+        "This log is supper fun should found",
+        "This log is supper fan should found",
+        "This log is supper fantastic should found",
+        "This log is supper fonetic should found",
+        "This log is supper fixed should found",
+        "This log is supper fu should found",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    let time_to = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+
+    sleep(Duration::from_millis(100));
+
+    let path = "http://0.0.0.0:8000/read";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&Query {
+            prefix: None,
+            words: None,
+            from: time_from.as_nanos() as u64,
+            to: time_to.as_nanos() as u64,
+        });
+    match status {
+        Ok(resp) => {
+            assert_eq!(resp.status(), 200);
+            let logs: LogsOutput = resp.into_json()?;
+            assert_eq!(logs.logs.len(), 7);
+            for w in "should found".split_whitespace() {
+                for log in logs.logs.iter() {
+                    let sl: Vec<String> = log.split_whitespace().map(str::to_string).collect();
+                    let Some(_) = sl.iter().find(|s| *s == w) else {
+                        assert!(false);
+                        return Err(Error::new(std::io::ErrorKind::NotFound, "item not found"));
+                    };
+                }
+            }
+        }
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn on_read_log_api_call_match_should_return_empty_result_for_time_rang_with_no_matching_logs(
+) -> Result<()> {
+    let path = "http://0.0.0.0:8000/save";
+
+    let time_from = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    sleep(Duration::from_millis(10));
+    let time_to = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default();
+    sleep(Duration::from_millis(10));
+    for l in vec![
+        "This log is super funny",
+        "This log is supper fun",
+        "This log is supper fan",
+        "This log is supper fantastic",
+        "This log is supper fonetic",
+        "This log is supper fixed",
+        "This log is supper fu",
+    ] {
+        let status = ureq::post(&path)
+            .set("Content-Type", "application/json")
+            .send_json(&LogInput { log: l.to_string() });
+        match status {
+            Ok(resp) => assert_eq!(resp.status(), 200),
+            Err(e) => {
+                println!("err {}", e);
+                assert!(false);
+            }
+        };
+        sleep(Duration::from_millis(WAIT_MS));
+    }
+
+    sleep(Duration::from_millis(100));
+
+    let path = "http://0.0.0.0:8000/read";
+
+    let status = ureq::post(&path)
+        .set("Content-Type", "application/json")
+        .send_json(&Query {
+            prefix: None,
+            words: None,
+            from: time_from.as_nanos() as u64,
+            to: time_to.as_nanos() as u64,
+        });
+    match status {
+        Ok(resp) => {
+            assert_eq!(resp.status(), 200);
+            let logs: LogsOutput = resp.into_json()?;
+            assert_eq!(logs.logs.len(), 0);
+        }
+        Err(e) => {
+            println!("err {}", e);
+            assert!(false);
+        }
+    };
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
 fn integration_bench_create_log() -> Result<()> {
     let path = "http://0.0.0.0:8000/save";
 
@@ -91,6 +620,7 @@ fn integration_bench_create_log() -> Result<()> {
 
     Ok(())
 }
+
 #[test]
 #[ignore]
 fn integration_bench_read_log() -> Result<()> {
